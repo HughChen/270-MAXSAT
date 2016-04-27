0aced79fe55c959f2e0250a410450aa42e6569e0
yiwen song <songzgy@gmail.com>
2016-04-27 16:46:17 -0700

    functionalized rand_lp_solver.py

--- rand_lp_solver.py
+++ rand_lp_solver.py
@@ -4,38 +4,36 @@ from scipy.optimize import linprog
 
 import numpy as np
 
-# Load clauses
-(VARDICT,clauses) = parse_file('1.in')
-keys = list(VARDICT.keys())
-keys.sort()
+def lp_solve(input_file):
+  # Load clauses
+  (VARDICT,clauses) = parse_file(input_file)
+  keys = list(VARDICT.keys())
+  keys.sort()
 
-# Construct the LP
+  # Construct the LP
 
-# >>> A = [[-3, 1], [1, 2]]
-# >>> b = [6, 4]
-# >>> x0_bounds = (None, None)
-# >>> x1_bounds = (-3, None)
-# >>> from scipy.optimize import linprog
-# >>> res = linprog(c, A_ub=A, b_ub=b, bounds=(x0_bounds, x1_bounds),
-# ...               options={"disp": True})
+  # >>> A = [[-3, 1], [1, 2]]
+  # >>> b = [6, 4]
+  # >>> x0_bounds = (None, None)
+  # >>> x1_bounds = (-3, None)
+  # >>> from scipy.optimize import linprog
+  # >>> res = linprog(c, A_ub=A, b_ub=b, bounds=(x0_bounds, x1_bounds),
+  # ...               options={"disp": True})
 
-num_cls = len(clauses)
-num_var = len(VARDICT.keys())
-c = -np.append(np.ones(num_cls), np.zeros(num_var))
-A = np.zeros((num_cls,num_cls+num_var))
-b = np.zeros(num_cls)
-for i in range(0,len(clauses)):
-  split_clause = clauses[i][2:-2].split(' or ')
-  A[i,i] = 1
-  print split_clause
-  for j in range(0,len(split_clause)):
-  	col = num_cls + keys.index(split_clause[j][-3])
-  	if 'not' in split_clause[j]:
-  		b[i] += 1
-  		A[i,col] = 1
-  	else:
-  		A[i,col] = -1
-print A
-print b
-res = linprog(c, A_ub=A, b_ub=b, bounds=(0, 1), options={"disp": True})
-print res
+  num_cls = len(clauses)
+  num_var = len(VARDICT.keys())
+  c = -np.append(np.ones(num_cls), np.zeros(num_var))
+  A = np.zeros((num_cls,num_cls+num_var))
+  b = np.zeros(num_cls)
+  for i in range(0,len(clauses)):
+    split_clause = clauses[i][2:-2].split(' or ')
+    A[i,i] = 1
+    print split_clause
+    for j in range(0,len(split_clause)):
+      col = num_cls + keys.index(split_clause[j][-3])
+      if 'not' in split_clause[j]:
+          b[i] += 1
+          A[i,col] = 1
+      else:
+          A[i,col] = -1
+  res = linprog(c, A_ub=A, b_ub=b, bounds=(0, 1), options={"disp": True})
