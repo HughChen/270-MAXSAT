8a5d10f7cdae8a09ce8fc5a76dcbaefdc17d8d4b
Hugh Chen <hugh.chen1@gmail.com>
2016-05-11 13:22:41 -0700

    Changed VARDICT from global to output

--- rand_lp_solver.py
+++ rand_lp_solver.py
@@ -6,6 +6,8 @@ import random
 
 import numpy as np
 
+VARDICT = {}
+
 def lp_solve(input_file):
   # Load clauses
   global VARDICT
@@ -14,15 +16,6 @@ def lp_solve(input_file):
   keys.sort()
 
   # Construct the LP
-
-  # >>> A = [[-3, 1], [1, 2]]
-  # >>> b = [6, 4]
-  # >>> x0_bounds = (None, None)
-  # >>> x1_bounds = (-3, None)
-  # >>> from scipy.optimize import linprog
-  # >>> res = linprog(c, A_ub=A, b_ub=b, bounds=(x0_bounds, x1_bounds),
-  # ...               options={"disp": True})
-
   num_cls = len(clauses)
   num_var = len(VARDICT.keys())
   c = -np.append(np.ones(num_cls), np.zeros(num_var))
@@ -31,7 +24,6 @@ def lp_solve(input_file):
   for i in range(0,len(clauses)):
     split_clause = clauses[i][2:-2].split(' or ')
     A[i,i] = 1
-    # print split_clause
     for j in range(0,len(split_clause)):
       col = num_cls + keys.index(split_clause[j][-3])
       if 'not' in split_clause[j]:
@@ -43,8 +35,6 @@ def lp_solve(input_file):
 
   rround = lambda p: True if random.random() < p else False
   x = np.array(map(rround,res['x']))
-  # print res['x']
-  # print x
   x = x[-num_var:]
   
   idx = 0
@@ -56,15 +46,18 @@ def rand_solve(input_file):
   global VARDICT
   (VARDICT,clauses) = parse_file(input_file)
   randbin = lambda: True if random.randint(0,1) == 1 else False
-
   for k in VARDICT.keys():
     VARDICT[k] = randbin()
 
-  # done
-
 def rand_lp_solve(input_file):
+  # Not sure if we have to round per clause or not
   coin = random.randint(0,1)
   if coin == 1:
     lp_solve(input_file)
   else:
     rand_solve(input_file)
+  print VARDICT
+  return VARDICT
+
+
+
